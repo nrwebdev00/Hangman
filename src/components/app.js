f13ae574ea8402a58fc41bee24a4e499f88777c2
Nathon Reed <nrwebdev00@gmail.com>
2020-05-07 10:35:23 -0700

    added custom scss mixins and setup styles for porject, added back image to hangman Screen

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
+import HangmanScreen from './hangmanScreen';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <HangmanScreen />
       </div>
     );
   }
---
+++ src/components/hangmanScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export default class HangmanScreen extends Component {
+    constructor(props){
+        super(props);
+
+       
+    }
+
+    render(){
+        return(
+            <div className="game-screen-wrapper">
+                Hello Hangman screen.
+            </div>
+        )
+    }
+}
